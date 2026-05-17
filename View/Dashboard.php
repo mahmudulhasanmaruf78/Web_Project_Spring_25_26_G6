656d923fd90db1df1b685004940c61f42130fa71
DibajitRoy <dibajit.2003@gmail.com>
2026-05-17 17:15:07 +0600

    Add files via upload
    dashboard

---
+++ View/Dashboard.php
@@ -0,0 +1,67 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["user_id"]))
+{
+    header("Location:Login.php");
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="../CSS/style.css">
+                              
+    </head>
+
+<body>
+    <div class="dashboard">
+
+<h1>Dashboard</h1>
+
+<br>
+
+<?php
+
+echo "Welcome ";
+
+echo $_SESSION["name"];
+
+?>
+
+<br><br>
+
+<?php
+
+echo "Role : ";
+
+echo $_SESSION["role"];
+
+?>
+
+<br><br>
+
+<h3>Profile Incomplete</h3>
+
+<a href="CompleteProfile.php">
+
+Complete Your Profile
+
+</a>
+
+<br><br>
+
+<a href="../Controller/Logout.php">
+
+Logout
+
+</a>
+
+</div>
+
+</body>
+
+</html>

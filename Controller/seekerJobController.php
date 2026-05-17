229c0a1e9029f1b78cc94bf2e113eb2a154b339f
professor4044 <mejamirsheikh@gmail.com>
2026-05-17 21:41:50 +0600

    seekerjobcontroller

---
+++ Controller/seekerJobController.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+require_once '../config/database.php';
+require_once '../Model/job.php';
+require_once '../Model/SavedJob.php';
+
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'seeker') {
+    header('Location: ../View/login.php');
+    exit;
+}
+
+$jobModel      = new Job($conn);
+$savedJobModel = new SavedJob($conn);
+
+$action = $_GET['action'] ?? 'index';
+
+if ($action === 'index') {
+    $jobs       = $jobModel->getActiveJobs();
+    $categories = $jobModel->getCategories();
+    $savedIds   = $savedJobModel->getSavedJobIds($_SESSION['user_id']);
+
+    require_once '../View/job_board.php';
+}
+
+elseif ($action==='show') {
+    $job_id = $_GET['id'];
+    $job    = $jobModel->getJobById($job_id);
+
+    if (!$job) {
+        die ('No job found.');
+    }
+
+    require_once '../Model/Application.php';
+    $appModel = new Application($conn);
+
+    $hasApplied = $appModel->hasApplied($job_id, $_SESSION['user_id']);
+
+    $isSaved = $savedJobModel->isSaved($_SESSION['user_id'], $job_id);
+
+    require_once '../View/job_detail.php';
+}
+else{
+    header('Location: ?action=index');
+    exit;
+}
+?>
+

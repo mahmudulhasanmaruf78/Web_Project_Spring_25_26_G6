dc0d1cb87dee209d174380b07d497bde28890636
DibajitRoy <dibajit.2003@gmail.com>
2026-05-17 17:18:51 +0600

    admin

---
+++ View/AdminDashboard.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["user_id"]))
+{
+    header("Location:Login.php");
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<link rel="stylesheet"
+href="../CSS/style.css">
+
+</head>
+
+<body>
+
+<div class="formbox">
+
+<h1>Admin Dashboard</h1>
+
+<h3>
+
+Welcome
+
+<?php
+echo $_SESSION["name"];
+?>
+
+</h3>
+
+<br>
+
+<a href="../Controller/Logout.php">
+
+Logout
+
+</a>
+
+</div>
+
+</body>
+
+</html>
